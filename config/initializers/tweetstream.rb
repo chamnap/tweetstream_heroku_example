4c8822208531ffc2d2a6f66906025012037e4904
chamnap <chamnapchhorn@gmail.com>
2013-04-13 21:20:20 +0700

    use environment variable

--- config/initializers/tweetstream.rb
+++ config/initializers/tweetstream.rb
@@ -1,9 +1,9 @@
 require 'tweetstream'
 
 TweetStream.configure do |config|
-  config.consumer_key       = "CpQRGSIEQOelSGQci7YRTQ"
-  config.consumer_secret    = "jq9rGXRbzKpOM079q6QC2mlPNsrkojhIuZiOPO1VkQs"
-  config.oauth_token        = "14653933-ZDyNKAXEAgm9sCEaLx2CJEjjEV6ualEfYPpf3b7ex"
-  config.oauth_token_secret = "vEP7uHLGkMxgt4bTrZy0Tx4dNdfwaswK5FfZBjDI"
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
   config.auth_method        = :oauth
 end
